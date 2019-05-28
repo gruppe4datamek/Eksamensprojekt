303841679ecc5b991d3b32e459edcfa25827747a
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2019-05-28 10:20:13 +0200

    Revert "Slettet alt unødvendigt bras"
    This reverts commit 62ce09959f02af3a38a947921c89259a64bcfe7d.

---
+++ Eksamensprojekt/Model/Artefakt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensprojekt
+{
+    
+    public class Artefakt
+    {
+        private string _navn;
+
+       
+        public Artefakt(string navn)
+        {
+            Navn = navn;
+        }
+        public string Navn { get => _navn; set => _navn = value; }
+
+
+    }
+}
---
+++ Eksamensprojekt/Model/Beskrivelse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensprojekt
+{
+    public class Beskrivelse
+    {
+        private int _beskrivelseId;
+        private string _beskrivelseNavn;
+        private string _beskrivelsesText;
+
+        public Beskrivelse(int id, string navn, string beskrivelse)
+        {
+            _beskrivelseId = id;
+            _beskrivelseNavn = navn;
+            _beskrivelsesText = beskrivelse;
+        }
+
+        public int BeskrivelseId
+        {
+            get { return _beskrivelseId; }
+            set { _beskrivelseId = value; }
+        }
+
+        public string BeskrivelseNavn
+        {
+            get { return _beskrivelseNavn; }
+            set { _beskrivelseNavn = value; }
+        }
+
+        public string BeskrivelsesText
+        {
+            get { return _beskrivelsesText; }
+            set { _beskrivelsesText = value; }
+        }
+    }
+}
